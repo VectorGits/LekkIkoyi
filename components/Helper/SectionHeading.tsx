202d963188183b094dfdde8cd895ebd5180067da
VectorGits <davidoluremi0@gmail.com>
2024-12-21 10:32:18 +0100

    update SectionHeading component with a new description for property selection

--- components/Helper/SectionHeading.tsx
+++ components/Helper/SectionHeading.tsx
@@ -9,7 +9,7 @@ const SectionHeading = ({heading} : Props) => {
   return (
     <div>
       <h1 className='text-2xl sm:text-3xl font-bold mb-2 text-gray-800'>{heading}</h1>
-      <p className='text-sm text-gray-700'>Lorem ipsum dolor sit amet.</p>
+      <p className='text-sm text-gray-700'>Explore our curated selection of properties across Lagos, including Ikoyi, Lekki, Victoria Island, and Epe. Start your search today!</p>
     </div>
   )
 }
